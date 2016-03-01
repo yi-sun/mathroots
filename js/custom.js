02f1dd76b4e4f54b4075f1771346b34f81c50aaa
yisun <yisun@mit.edu>
2016-03-01 16:36:54 -0500

    Remove sidebar js

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,3 @@
 $(document).on('scroll', function (e) {
     $('.navbar').css('background-color', 'rgba(0, 0, 0, ' + $(document).scrollTop() / 100 + ')');
 });
-
-
-$('body').scrollspy({
-    target: '.bs-docs-sidebar'
-})
-
-$('#sidebar').affix({
-    offset: {     
-      top: $('#sidebar').offset().top
-    }
-});
